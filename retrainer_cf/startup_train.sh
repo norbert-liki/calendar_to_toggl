0c28231783ac1099b3c98298630ac8890268b6c2
norbert-liki <47354641+norbert-liki@users.noreply.github.com>
2021-03-22 13:40:51 +0100

    fixing branch in startup script

--- retrainer_cf/startup_train.sh
+++ retrainer_cf/startup_train.sh
@@ -12,7 +12,7 @@ useradd -m -d /home/pythonapp pythonapp
 
 # Fetch source code
 export HOME=/root
-git clone -b ml_pred https://github.com/norbert-liki/calendar_to_toggl.git /opt/app
+git clone https://github.com/norbert-liki/calendar_to_toggl.git /opt/app
 
 # Python environment setup
 cd /opt/app
